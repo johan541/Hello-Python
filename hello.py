852c8dd90bf8ede2ea1ea3fac0b807b47e468106
Johan Garcia <js.garcia@udla.edu.co>
2024-04-19 22:10:59 +0000

    first class python

---
+++ hello.py
@@ -0,0 +1,24 @@
+# hello word comilla simple
+print('hello python')
+# hello word comilla doble
+print("hello python")
+
+# addition
+print(3+2)
+# subtraction
+print(3-2)
+# multiplication
+print(3*2)
+# division
+print(3/2)
+# exponential
+print(3**2)
+# floor division operator
+print(3//2)
+
+# prin data type
+print(type("hello python")) # str
+print(type(5)) # int
+print(type(5.2)) # float
+print(type(True)) # bool
+print(type(5 + 2j)) # complex
